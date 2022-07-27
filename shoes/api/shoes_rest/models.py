d84ba8a8f23d6fb80858376896c1454b8143f0da
Alex <alexleeadams@aol.com>
2022-07-27 11:05:26 -0500

    updated models

--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Shoe(models.model):
+    manufacturer = models.CharField(max_length=200, null=True, blank = True)
+    model_name = models.CharField(max_length=200, null=True, blank = True)
+    color = models.CharField(max_length=200, null=True, blank = True)
+    picture_url = models.URLfield(null=True)
