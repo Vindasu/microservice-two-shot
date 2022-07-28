fca253bd09dac56c3105c9792304a0bb4407fd69
edwardlocnguyen <edwardlocnguyen@gmail.com>
2022-07-28 15:19:37 -0500

    accepted the damn cahnges

--- ghi/app/src/HatsList.js
+++ ghi/app/src/HatsList.js
@@ -50,7 +50,6 @@ export default () => {
 
 // export default HatsList;
 
-<<<<<<< HEAD
 // async function loadHats() {
 //   const response = await fetch('http://localhost:8090/api/hats_rest/');
 //   if (response.ok) {
@@ -66,40 +65,3 @@ export default () => {
 //   }
 // }
 // loadHats();
-=======
-function HatsList() {
-  const [hats, setHats] = useState([])
-  return (
-    <table className="table table-striped">
-      <thead>
-        <tr>
-          <th>Hat Fabric</th>
-          <th>Hat Style</th>
-          <th>Hat Color</th>
-          <th>Hat Picture</th>
-          <th>Location</th>
-        </tr>
-      </thead>
-      <tbody>
-        {hats.map(hat => {
-          return (
-            <tr key={hat.id}>
-              <td>{ hat.fabric }</td>
-              <td>{ hat.style_name }</td>
-              <td>{ hat.color }</td>
-              {/* <td>
-              <img src={hat.picture_url} className="" alt= "..."></img>
-              </td> */}
-              <td>{ hat.picture_url }</td>
-              <td>{ hat.location }</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
-}
-  
-export default HatsList;
-  
->>>>>>> refs/remotes/origin/main
