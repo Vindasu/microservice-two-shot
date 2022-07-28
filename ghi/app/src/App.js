80309f8eb4277fcb173fe6b31e80d385e8c730f8
edwardlocnguyen <edwardlocnguyen@gmail.com>
2022-07-28 14:37:00 -0500

    made updates to hatlist

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -5,24 +5,15 @@ import HatForm from './HatForm';
 import HatsList from './HatsList';
 import ShoeForm from './ShoeForm';
 
-function App(props) {
-  if (props.hats === undefined) {
-    return null;
-  }
+function App() {
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
-        </Routes>
-        <Routes>
-          <Route path="/hats" element={<HatsList hats={props.hats} />} />
-        </Routes>
-        <Routes>
+          <Route path="/hats" element={<HatsList />} />
           <Route path="/hats/new" element={<HatForm />} />
-        </Routes>
-        <Routes>
           <Route path="/shoes/new" element={<ShoeForm />} />
         </Routes>
       </div>
--- ghi/app/src/HatsList.js
+++ ghi/app/src/HatsList.js
@@ -1,60 +1,55 @@
 import React from 'react';
 import App from './App';
-import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-    <App />
-    </React.StrictMode>
-);
-async function loadHats() {
-  const response = await fetch('http://localhost:8090/api/hats_rest/');
-  if (response.ok) {
-    const data = await response.json();
-    root.render(
-      <React.StrictMode>
-        <App hats={data.hats} />
+// async function loadHats() {
+//   const response = await fetch('http://localhost:8090/api/hats_rest/');
+//   if (response.ok) {
+//     const data = await response.json();
+//     root.render(
+//       <React.StrictMode>
+//         <App hats={data.hats} />
 
-      </React.StrictMode>
-    );
-  } else {
-    console.error(response);
-  }
+//       </React.StrictMode>
+//     );
+//   } else {
+//     console.error(response);
+//   }
+// }
+// loadHats();
+
+function HatsList() {
+  const [hats, setHats] = useState([])
+  return (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>Hat Fabric</th>
+          <th>Hat Style</th>
+          <th>Hat Color</th>
+          <th>Hat Picture</th>
+          <th>Location</th>
+        </tr>
+      </thead>
+      <tbody>
+        {hats.map(hat => {
+          return (
+            <tr key={hat.id}>
+              <td>{ hat.fabric }</td>
+              <td>{ hat.style_name }</td>
+              <td>{ hat.color }</td>
+              {/* <td>
+              <img src={hat.picture_url} className="" alt= "..."></img>
+              </td> */}
+              <td>{ hat.picture_url }</td>
+              <td>{ hat.location }</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
-loadHats();
-
-function HatsList(props) {
-    return (
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Hat Fabric</th>
-            <th>Hat Style</th>
-            <th>Hat Color</th>
-            <th>Hat Picture</th>
-            <th>Location</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.hats.map(hat => {
-            return (
-              <tr key={hat.id}>
-                <td>{ hat.fabric }</td>
-                <td>{ hat.style_name }</td>
-                <td>{ hat.color }</td>
-                {/* <td>
-                <img src={hat.picture_url} className="" alt= "..."></img>
-                </td> */}
-                <td>{ hat.picture_url }</td>
-                <td>{ hat.location }</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    );
-  }
   
   export default HatsList;
   
