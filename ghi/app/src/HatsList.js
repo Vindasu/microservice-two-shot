ed2fa7f191495ca95530aecf406c9e558ef9762f
edwardlocnguyen <edwardlocnguyen@gmail.com>
2022-07-28 15:15:27 -0500

    hats list changes

--- ghi/app/src/HatsList.js
+++ ghi/app/src/HatsList.js
@@ -1,5 +1,54 @@
-import React, { useState } from 'react';
-import App from './App';
+import React, { useState, useEffect } from 'react';
+// import App from './App';
+
+export default () => {
+  const [hats, setHats] = useState([])
+
+  const fetchHats = async () => {
+    const url = 'http://localhost:8090/api/hats_rest/'
+    const res = await fetch(url)
+    const hatsJSON = await res.json()
+    setHats(hatsJSON.hats);
+  }
+
+  useEffect(() => {
+    fetchHats()
+  }, [])
+
+  return (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>Hat Fabric</th>
+          <th>Hat Style</th>
+          <th>Hat Color</th>
+          <th>Hat Picture</th>
+          <th>Location</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        {hats.map(hat => {
+          return (
+            <tr key={hat.id}>
+              <td>{ hat.fabric }</td>
+              <td>{ hat.style_name }</td>
+              <td>{ hat.color }</td>
+              {/* <td>
+              <img src={hat.picture_url} className="" alt= "..."></img>
+            </td> */}
+              <td>{ hat.picture_url }</td>
+              <td>{ hat.location }</td>
+              <td>Delete</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+// export default HatsList;
 
 // async function loadHats() {
 //   const response = await fetch('http://localhost:8090/api/hats_rest/');
@@ -16,39 +65,3 @@ import App from './App';
 //   }
 // }
 // loadHats();
-
-function HatsList() {
-  const [hats, setHats] = useState([])
-  return (
-    <table className="table table-striped">
-      <thead>
-        <tr>
-          <th>Hat Fabric</th>
-          <th>Hat Style</th>
-          <th>Hat Color</th>
-          <th>Hat Picture</th>
-          <th>Location</th>
-        </tr>
-      </thead>
-      <tbody>
-        {hats.map(hat => {
-          return (
-            <tr key={hat.id}>
-              <td>{ hat.fabric }</td>
-              <td>{ hat.style_name }</td>
-              <td>{ hat.color }</td>
-              {/* <td>
-              <img src={hat.picture_url} className="" alt= "..."></img>
-              </td> */}
-              <td>{ hat.picture_url }</td>
-              <td>{ hat.location }</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
-}
-  
-export default HatsList;
-  
